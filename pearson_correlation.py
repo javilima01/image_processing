c540a7ecaea446894555b9da7b1f60193322718d
javilima01 <javierbilbao75@gmail.com>
2024-03-21 22:40:03 +0100

    JaBi: added GUI

--- pearson_correlation.py
+++ pearson_correlation.py
@@ -4,8 +4,28 @@ import skimage.filters as skf
 import skimage.measure as skm
 import skimage.draw as skd
 import matplotlib.pyplot as plt
-import argparse
+import tkinter as tk
+from tkinter import filedialog
 
+class GUI:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Image Analysis")
+
+        self.label = tk.Label(root, text="Select an image:")
+        self.label.pack()
+
+        self.btn_browse = tk.Button(root, text="Browse", command=self.browse_image)
+        self.btn_browse.pack()
+
+        self.img_path = None
+
+        self.processor = ImageProcessing()
+
+    def browse_image(self):
+        self.img_path = filedialog.askopenfilename()
+        if self.img_path:
+            self.processor.process_image(self.img_path)
 
 class ImageProcessing:
 
@@ -64,85 +84,75 @@ class ImageProcessing:
         cropped_image = image[top:bottom, left:right]
 
         return cropped_image
-    
-def parse_args():
-    parser = argparse.ArgumentParser(
-        description="Program to calculate pearson correlation of images."
-    )
 
-    # Add command line arguments
-    parser.add_argument(
-        "--image", "-i", type=str, required=True, help="Image to analyze"
-    )
+    def process_image(self, img_path):
+        data = {
+            "green": {"coordinates": [], "pcc": [], "contour": []},
+            "red": {"coordinates": [], "pcc": [], "contour": []},
+        }
 
-    return parser.parse_args()
+        # Read the image
+        img = skio.imread(img_path)
+
+        # Split the image into channels
+        red = img[:, :, 0]
+        green = img[:, :, 1]
+
+        # Fill holes in the red and green channels and get contours
+        red_filled, red_contours = self.fill_image(red)
+        green_filled, green_contours = self.fill_image(green)
+
+        # Filter contours based on size (area)
+        min_contour_area = 1000  # Adjust as needed
+        red_contours = [props for props in red_contours if props.area > min_contour_area]
+        green_contours = [
+            props for props in green_contours if props.area > min_contour_area
+        ]
+
+        # Fill different figures using contours and compute Pearson correlation coefficient for each
+        for props in red_contours:
+            green_cropped = self.crop_image(green_filled, props)
+            red_cropped = self.crop_image(red_filled, props)
+            pcc = self.get_pearson(red_cropped, green_cropped)
+            data["red"]["pcc"].append(pcc)
+            data["red"]["contour"].append(props)
+
+            centroid = props.centroid
+            data["red"]["coordinates"].append((centroid[0], centroid[1]))
+
+        for props in green_contours:
+            green_cropped = self.crop_image(green_filled, props)
+            red_cropped = self.crop_image(red_filled, props)
+            pcc = self.get_pearson(green_cropped, red_cropped)
+            data["green"]["pcc"].append(pcc)
+            data["green"]["contour"].append(props)
+
+            centroid = props.centroid
+            data["green"]["coordinates"].append((centroid[0], centroid[1]))
+
+        self.figure_pcc(red_filled, data["red"])
+
+        self.figure_pcc(green_filled, data["green"])
+
+        # Create red and green masks with the same shape as the original image
+        red_mask = np.zeros_like(img)
+        green_mask = np.zeros_like(img)
+
+        for props in red_contours:
+            rr, cc = skd.polygon(props.coords[:, 0], props.coords[:, 1])
+            red_mask[rr, cc, 0] = red[rr, cc]
+
+        for props in green_contours:
+            rr, cc = skd.polygon(props.coords[:, 0], props.coords[:, 1])
+            green_mask[rr, cc, 1] = green[rr, cc]
+
+        self.figure_pcc(red_mask, data["red"])
+        self.figure_pcc(green_mask, data["green"])
+
+        plt.show()
 
 
 if __name__ == "__main__":
-    args = parse_args()
-    img = args.image
-
-    data = {
-        "green": {"coordinates": [], "pcc": [], "contour": []},
-        "red": {"coordinates": [], "pcc": [], "contour": []},
-    }
-    processor = ImageProcessing()
-    # Read the image
-    img = skio.imread(img)
-
-    # Split the image into channels
-    red = img[:, :, 0]
-    green = img[:, :, 1]
-
-    # Fill holes in the red and green channels and get contours
-    red_filled, red_contours = processor.fill_image(red)
-    green_filled, green_contours = processor.fill_image(green)
-
-    # Filter contours based on size (area)
-    min_contour_area = 1000  # Adjust as needed
-    red_contours = [props for props in red_contours if props.area > min_contour_area]
-    green_contours = [
-        props for props in green_contours if props.area > min_contour_area
-    ]
-
-    # Fill different figures using contours and compute Pearson correlation coefficient for each
-    for props in red_contours:
-        green_cropped = processor.crop_image(green_filled, props)
-        red_cropped = processor.crop_image(red_filled, props)
-        pcc = processor.get_pearson(red_cropped, green_cropped)
-        data["red"]["pcc"].append(pcc)
-        data["red"]["contour"].append(props)
-
-        centroid = props.centroid
-        data["red"]["coordinates"].append((centroid[0], centroid[1]))
-
-    for props in green_contours:
-        green_cropped = processor.crop_image(green_filled, props)
-        red_cropped = processor.crop_image(red_filled, props)
-        pcc = processor.get_pearson(green_cropped, red_cropped)
-        data["green"]["pcc"].append(pcc)
-        data["green"]["contour"].append(props)
-
-        centroid = props.centroid
-        data["green"]["coordinates"].append((centroid[0], centroid[1]))
-
-    processor.figure_pcc(red_filled, data["red"])
-
-    processor.figure_pcc(green_filled, data["green"])
-
-    # Create red and green masks with the same shape as the original image
-    red_mask = np.zeros_like(img)
-    green_mask = np.zeros_like(img)
-
-    for props in red_contours:
-        rr, cc = skd.polygon(props.coords[:, 0], props.coords[:, 1])
-        red_mask[rr, cc, 0] = red[rr, cc]
-
-    for props in green_contours:
-        rr, cc = skd.polygon(props.coords[:, 0], props.coords[:, 1])
-        green_mask[rr, cc, 1] = green[rr, cc]
-
-    processor.figure_pcc(red_mask, data["red"])
-    processor.figure_pcc(green_mask, data["green"])
-
-    plt.show()
+    root = tk.Tk()
+    app = GUI(root)
+    root.mainloop()
